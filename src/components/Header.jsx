773e2ae73579faa5e08a5c4af9332bd25a6545ec
sosaneitor <sss_sosa@hotmail.com>
2022-08-17 09:19:23 -0500

    Primer y ultimo commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import NuevoPresupuesto from './NuevoPresupuesto'
+import ControlPresupuesto from './ControlPresupuesto'
+
+const Header = ({
+    gastos,
+    setGastos,
+    presupuesto,
+    setPresupuesto, 
+    valido, 
+    setValido}) => {
+  return (
+    <header>
+        <h1>Planificador de Gastos </h1>
+
+        {valido ? (
+            <ControlPresupuesto
+                gastos={gastos}
+                setGastos={setGastos}
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+                setValido={setValido}
+            
+            /> 
+        ):(
+            
+            <NuevoPresupuesto
+             presupuesto={presupuesto}
+             setPresupuesto={setPresupuesto}
+             setValido={setValido}
+            />
+        )}
+
+    </header>
+  )
+}
+
+export default Header
